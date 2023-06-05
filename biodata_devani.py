b8a983b4af3a87e5460ca096bd0de6656ed04259
DevaniPramitaDewi <126579932+DevaniPramitaDewi@users.noreply.github.com>
2023-06-05 09:26:31 +0800

    Add files via upload

---
+++ biodata_devani.py
@@ -0,0 +1,17 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "Ni Luh Putu Devani Pramita Dewi"
+hobi = "Jalan-jalan"
+alamat = "Desa Karyasari, Pupuan"
+Kota = "Tabanan"
+Propinsi = "Bali"
+
+print("Nama Lengkap  :",nama)
+print("Alamat        :",alamat, "\n                Kota ", Kota,"Propinsi", Propinsi)
+print("Hobi          :", hobi)
+
+print ("Selamat Datang di Biodataku")
+print ("=======================================")
+ 
