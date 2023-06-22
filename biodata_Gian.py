2406f6a816d92472ae680f9391e88349bcd144db
giaann23 <andika.cjr123@gmail.com>
2023-06-22 10:55:37 +0700

    biodata_gian

---
+++ biodata_Gian.py
@@ -0,0 +1,13 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+
+nama = "Gian Lucky Al Hadi"
+hobi = "Bermain Game"
+alamat = "Sindanglaka Karangtengah Cianjur"
+kota = "Cianjur"
+
+print("Nama Lengkap  :",nama)
+print("Alamat        :"alamat, "\                   Kota ", Kota)
+print("Hobi          :, hobi)
+
+print ("======================================")
